6b2cb4c84fac578031ecc5bde62c767584b22b06
Chris Caskey <cdcaskey@users.noreply.github.com>
2018-06-02 18:58:08 +0100

    Created a UDP listener

---
+++ TelemetryViewer/TelemetryViewer.Server/Helpers/UdpListener.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelemetryViewer.Server.Helpers
+{
+    public sealed class UdpListener : IDisposable
+    {
+        private UdpClient _udpClient;
+
+        public event EventHandler<byte[]> DataReceived;
+
+        public UdpListener(int listenPort)
+        {
+            _udpClient = new UdpClient(listenPort);
+        }
+
+        public void StartListening()
+        {
+            _udpClient.BeginReceive(new AsyncCallback(result =>
+            {
+                // Get the Received Data
+                var endpoint = new IPEndPoint(IPAddress.Any, 0);
+                var data = _udpClient.EndReceive(result, ref endpoint);
+
+                StartListening();
+
+                // Send the data to any interested party
+                DataReceived?.Invoke(this, data);
+            }), null);
+        }
+
+        public void Dispose()
+        {
+            _udpClient?.Dispose();
+            _udpClient = null;
+        }
+    }
+}
