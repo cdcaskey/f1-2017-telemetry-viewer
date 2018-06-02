bffdf76c295a537988ca3aed7bbe02192445daeb
Chris Caskey <cdcaskey@users.noreply.github.com>
2018-06-02 11:12:57 +0100

    Created the server project

---
+++ TelemetryViewer/TelemetryViewer.Server/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelemetryViewer.Server
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if (Environment.UserInteractive)
+            {
+                new TelemetryViewerServer().OnCommandLineStart(args);
+
+                Console.WriteLine("The server is now running, press enter to exit...");
+                Console.ReadLine();
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                    new TelemetryViewerServer()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
---
+++ TelemetryViewer/TelemetryViewer.Server/TelemetryViewerServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelemetryViewer.Server
+{
+    public partial class TelemetryViewerServer : ServiceBase
+    {
+        public TelemetryViewerServer()
+        {
+            InitializeComponent();
+        }
+
+        internal void OnCommandLineStart(string[] args) => OnStart(args);
+
+        internal void OnCommandLineStop(string[] args) => OnStop();
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
